c46ca85feb418ddb6483036d86974c4c65e95901
xiexuanzheng123 <349328984@qq.com>
2017-02-15 17:51:57 +0800

    some demos of react

---
+++ html/firstdemo.js
@@ -0,0 +1,8 @@
+ReactDOM.render(
+        <div>
+            <h1>Hello,world!</h1>
+            <h2>欢迎学习！</h2>
+            <p data-myattribute = 'someValue'>这是一个很不错的JavaScript库！</p>
+        </div>,
+        document.getElementById('example')
+        );
