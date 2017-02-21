44d941b5ad0bf0b66b0844ce9a09efe976f33728
xiexuanzheng123 <349328984@qq.com>
2017-02-21 18:10:22 +0800

    test

--- html/firstdemo.js
+++
@@ -1,8 +0,0 @@
-ReactDOM.render(
-        <div>
-            <h1>Hello,world!</h1>
-            <h2>欢迎学习！</h2>
-            <p data-myattribute = 'someValue'>这是一个很不错的JavaScript库！</p>
-        </div>,
-        document.getElementById('example')
-        );
